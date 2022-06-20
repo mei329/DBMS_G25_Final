c92ddbf93faa637192bfc194dbc20ea78de7f44a
Mei329 <maggie10135@gmail.com>
2022-06-20 13:19:42 +0800

    Update order functions
    check status & modify user

--- src/Entity/Order.java
+++ src/Entity/Order.java
@@ -1,246 +1,101 @@
 package Entity;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
-import javax.swing.JOptionPane;
-
 public class Order extends ConnectDb {
-	
+	private Blacklist blck = new Blacklist();
+
 	public static void main(String[] args) {
 		Order o = new Order();
-		Object[] oid = o.select("order_id", "order_date", "'2022-06-09'");
-		for (int i = 0; i < oid.length; i++) {
-			Long id = (Long) oid[i];
-			o.delOrder(id);
-		}
-	}
 
-	// A. 討論室
-	public boolean createRoomOrder(int roomId, String oStatus, String orderDate, int borrow_start, int borrow_end,
-			ArrayList<Long> users) {
+		ArrayList<Long> users = new ArrayList<Long>();
+		users.add(109405007l);
 
-		long time = createRoomOrder(roomId, oStatus, orderDate, borrow_start, borrow_end);
-		if (time != -1) {
-			// 新增訂單使用者
-			boolean b = createOrderUsers(time, users);
-			if (b == true) {
-				return true;
-			} else {
-				// 刪除訂單
-				delOrder(time);
-				return false;
-			}
-		}
-		return false;
-	}
+		long oid = o.createOrder("A001", "2022-06-21", 9, 10, users);
+		System.out.println("createOrder: " + oid);
 
-	public long createRoomOrder(int roomId, String oStatus, String orderDate, int borrow_start, int borrow_end) {
+		System.out.println("checkSeat: " + o.checkSeat("A001", 109405007l));
 		
-		super.connect();
+		System.out.println("modifyUser: " + o.modifyUser(oid, 109405007l, 109405088l));
+
+		System.out.println("modifyUser: add a blacklist user: " + o.modifyUser(oid, 109405088l, 109306099l));
+
+		System.out.println("modifyTime: " + o.modifyTime(oid, "borrow_end", 12));
+
+		System.out.println("delOrder: " + oid + " " + o.delOrder(oid));
+
+		System.out.println("checkSeat: " + o.checkSeat("A001", 109405088l));
+
+		users.add(109405088l);
+		System.out.println("checkRoom: " + o.checkRoom("401", users));
+		users.add(109405094l);
+		System.out.println("checkRoom: " + o.checkRoom("401", users));
+		users.add(109306099l);
+		System.out.println("checkRoom: " + o.checkRoom("401", users));
+	}
+
+	public long createOrder(String seat_id, String orderDate, int borrow_start, int borrow_end, ArrayList<Long> users) {
+		// remember to call checkSeat or checkRoom method before placing order!!!
+
 		long timestamp = new Date().getTime();
 		String sql = "INSERT INTO "
-				+ "Orders(order_id , room_id , order_status , order_date , borrow_start , borrow_end) "
+				+ "Orders(order_id , seat_id , order_status , order_date , borrow_start , borrow_end) "
 				+ "VALUES(?,?,?,?,?,?)";
+		String sql2 = "INSERT INTO " + "OrderUsers(order_id , user_id) " + "VALUES(?,?)";
 
 		try {
+			super.connect();
+			// Update to Room
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setLong(1, timestamp);
-			pstmt.setInt(2, roomId);
-			pstmt.setString(3, oStatus);
+			pstmt.setString(2, seat_id);
+			pstmt.setString(3, "T");
 			pstmt.setString(4, orderDate);
 			pstmt.setInt(5, borrow_start);
 			pstmt.setInt(6, borrow_end);
-			pstmt.executeUpdate();
-			System.out.println("Create room order.");
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			return -1l;
-		} finally {
-			super.closeConn();
-		}
 
-		return timestamp;
-	}
-	
-
-	private boolean createOrderUsers(long order_id, ArrayList<Long> users) {
-		
-		super.connect();
-		for (Long uid : users) {
-			String sql = "INSERT INTO " + "OrderUsers( order_id , user_id) " + "VALUES(?,?)";
-			try {
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setLong(1, order_id);
-				pstmt.setLong(2, uid);
-				if (users.size() > 1 && uid == users.get(0)) {
-					// The 1st user_id in users is the lender.
-					pstmt.setString(3, "T");
+			// Update to OrderUsers
+			if (pstmt.executeUpdate() != 0) {
+				for (Long uid : users) {
+					pstmt = conn.prepareStatement(sql2);
+					pstmt.setLong(1, timestamp);
+					pstmt.setLong(2, uid);
+					if (users.size() > 1 && uid == users.get(0)) {
+						// The 1st user_id in users is the lender.
+						pstmt.setString(3, "T");
+					}
+					if (pstmt.executeUpdate() == 0) {
+						delOrder(timestamp);
+						return -1l;
+					}
 				}
-				pstmt.executeUpdate();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-				return false;
-			} finally {
-				super.closeConn();
-			}
-		}
-		return true;
-	}
-
-	// B. 自習室
-	public boolean createSeatOrder(String seatId, String oStatus, String orderDate, int borrow_start, int borrow_end,
-			long user_id) {
-
-		long time = createSeatOrder(seatId, oStatus, orderDate, borrow_start, borrow_end);
-		if (time != -1) {
-			 // 新增訂單使用者
-			ArrayList<Long> users = new ArrayList<Long>();
-			users.add(user_id);
-			boolean b = createOrderUsers(time, users);
-			if (b == true) {
-				return true;
-			} else {
-				// 刪除訂單
-				delOrder(time);
-				return false;
-			}
-		}
-		return false;
-	}
-
-	public long createSeatOrder(String seatId, String oStatus, String orderDate, int borrow_start, int borrow_end) {
-
-		super.connect();
-		long timestamp = new Date().getTime();
-		String sql = "INSERT INTO "
-				+ "Orders(order_id , seat_id , order_status , order_date , borrow_start , borrow_end ) "
-				+ "VALUES(?,?,?,?,?,?)";
-
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, timestamp);
-			pstmt.setString(2, seatId);
-			pstmt.setString(3, oStatus);
-			pstmt.setString(4, orderDate);
-			pstmt.setInt(5, borrow_start);
-			pstmt.setInt(6, borrow_end);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			return -1l;
-		} finally {
-			super.closeConn();
-		}
-
-		return timestamp;
-	}
-	
-	public Object[] select(String select, String attr, String value) {
-		// String select = one attribute that you want to select
-		super.connect();
-		String count = "SELECT COUNT(order_id) FROM Orders WHERE " + attr + " = " + value;
-		String query = "SELECT " + select + " FROM Orders WHERE " + attr + " = " + value;
-		
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet result = stmt.executeQuery(count);
-			int size = 0;
-			if (result.next()) {
-				size = result.getInt(1);
-			}
-			Object[] retVal = new Object[size];
-			int i = 0;
-			result = stmt.executeQuery(query);
-			while (result.next()) {
-				retVal[i] = result.getObject(1);
-				i++;
-			}
-			return retVal;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.closeConn();
-		}
-		return null;
-	}
-
-	public boolean delOrder(long order_id) {
-		
-		super.connect();
-		String sql = "DELETE from Orders where order_id = ?";
-		String sql2 = "DELETE from OrderUsers where order_id = ?";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, order_id);
-			int row = pstmt.executeUpdate();
-			pstmt = conn.prepareStatement(sql2);
-			pstmt.setLong(1, order_id);
-			int row2 = pstmt.executeUpdate();
-			
-			if (row != 0 && row2 != 0) {
-				System.out.println("DELETE from Orders AND OrderUsers where order_id = " + order_id + "  ==> SUCCESS!!");
-				return true;
+				updateSeatStatus(seat_id, "F");
+				updateUserStatus(users, "F");
+				return timestamp;
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+			delOrder(timestamp);
 		} finally {
 			super.closeConn();
 		}
-		
-		return false;
+		return -1l;
 	}
 
-	public boolean delUser(int order_id, int user_id) {
-
-		super.connect();
+	public boolean addUser(long order_id, long user_id) {
 		try {
-			String query = "DELETE FROM OrderUsers WHERE order_id = " + order_id + "AND user_id = " + user_id;
+			super.connect();
+			String query = "INSERT INTO OrderUsers(order_id, user_id) VALUES(" + order_id + ", " + user_id + ")";
 			Statement stmt = conn.createStatement();
-			stmt.execute(query);
-			return true;
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			super.closeConn();
-		}
-		return false;
-	}
-
-	public boolean addUser(int order_id, int user_id) {
-
-		super.connect();
-		try {
-			String query = "INSERT INTO OrderUsers VALUES(" + order_id + user_id + ")";
-			Statement stmt = conn.createStatement();
-			stmt.execute(query);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-		} finally {
-			super.closeConn();
-		}
-		return false;
-	}
-
-	public boolean modifyTime(int order_id, String attr, Date time) {
-		super.connect();
-		// attr == "borrow_start" or "borrow_end"
-		String getStart = "SELECT borrow_start FROM Orders WHERE order_id = " + order_id;
-		try {
-			Statement stmt = conn.createStatement();
-			boolean hasResultSet = stmt.execute(getStart);
-			if (hasResultSet == true) {
-				ResultSet result = stmt.getResultSet();
-				long borrowStart = result.getInt("borrow_start");
-				long now = System.currentTimeMillis();
-				if (Long.compare(borrowStart, now) > 0) {
-					String query = "UPDATE Orders SET " + attr + " = " + time + " WHERE order_id = " + order_id;
-					stmt.execute(query);
+			if (checkUserStatus(user_id) && !blck.isBanned(user_id)) {
+				if (stmt.executeUpdate(query) != 0) {
+					updateUserStatus(user_id, "F");
 					return true;
 				}
 			}
@@ -252,13 +107,176 @@ public class Order extends ConnectDb {
 		return false;
 	}
 
-	public boolean checkUserStatus(long user_id) {
-		Blacklist blck = new Blacklist();
-		if (blck.isBanned(user_id)) {
-			return false;
-		} else {
-			return true;
+	public boolean modifyUser(long order_id, long before_user, long after_user) {
+		// set before_user to after_user from OrderUsers
+		try {
+			super.connect();
+			String query = "UPDATE OrderUsers SET user_id = " + after_user + " WHERE user_id = " + before_user
+					+ " AND order_id = " + order_id;
+			Statement stmt = conn.createStatement();
+			if (checkUserStatus(after_user) && !blck.isBanned(after_user)) {
+				if (stmt.executeUpdate(query) != 0) {
+					updateUserStatus(before_user, "T");
+					updateUserStatus(after_user, "F");
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
 		}
+		return false;
+	}
+
+	public boolean modifyTime(long order_id, String attr, int time) {
+		// attr == "borrow_start" or "borrow_end"
+		try {
+			super.connect();
+			String query = "UPDATE Orders SET " + attr + " = " + time + " WHERE order_id = " + order_id;
+			Statement stmt = conn.createStatement();
+
+			String status = select("order_status", "Orders", "order_id", Long.toString(order_id))[0];
+			if (status.equals("T")) {
+				if (stmt.executeUpdate(query) != 0) {
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean delOrder(long order_id) {
+		// Delete from Orders and OrderUsers
+		try {
+			super.connect();
+			String query = "DELETE from Orders where order_id = " + order_id;
+			Statement stmt = conn.createStatement();
+
+			String[] seat_id = select("seat_id", "Orders", "order_id", Long.toString(order_id));
+
+			if (stmt.executeUpdate(query) != 0 && delUser(order_id) == true) {
+				if (seat_id.length != 0) {
+					updateSeatStatus(seat_id[0], "T");
+				}
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean delUser(long order_id) {
+		try {
+			super.connect();
+			String query = "DELETE FROM OrderUsers WHERE order_id = " + order_id;
+			Statement stmt = conn.createStatement();
+
+			String[] users = select("user_id", "OrderUsers", "order_id", Long.toString(order_id));
+
+			if (stmt.executeUpdate(query) != 0) {
+				for (String u : users) {
+					updateUserStatus(Long.parseLong(u), "T");
+				}
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean delUser(long order_id, long user_id) {
+		try {
+			super.connect();
+			String query = "DELETE FROM OrderUsers WHERE order_id = " + order_id + " AND user_id = " + user_id;
+			Statement stmt = conn.createStatement();
+			if (stmt.executeUpdate(query) != 0) {
+				updateUserStatus(user_id, "T");
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean updateSeatStatus(String place_id, String status) {
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			// update seat or room status
+			if (place_id.matches("^[ABC].*")) {
+				String updateSeat = "UPDATE Seat SET seat_status = '" + status + "' WHERE seat_id = '" + place_id + "'";
+				if (stmt.executeUpdate(updateSeat) != 0) {
+					return true;
+				}
+			} else {
+				String updateRoom = "UPDATE Room SET room_status = '" + status + "' WHERE room_id = " + place_id + "";
+				if (stmt.executeUpdate(updateRoom) != 0) {
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean updateUserStatus(long user_id, String status) {
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			// update user status
+			String updateUser = "UPDATE User SET user_status = '" + status + "' WHERE user_id = " + user_id;
+			if (stmt.executeUpdate(updateUser) != 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
+	}
+
+	public boolean updateUserStatus(ArrayList<Long> users, String status) {
+		for (Long uid : users) {
+			if (!updateUserStatus(uid, status)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean checkUserStatus(long user_id) {
+		String query = "SELECT user_status FROM User WHERE user_id = " + user_id + " AND user_status = 'T'";
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			if (stmt.executeQuery(query).next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
+		return false;
 	}
 
 	public boolean checkSeatStatus(String seat_id) {
@@ -277,45 +295,29 @@ public class Order extends ConnectDb {
 		return false;
 	}
 
-	public boolean checkAttendance() {
-		// check minimum attendance
-
-		// check maximum attendance
+	public boolean checkRoomStatus(String room_id) {
+		String query = "SELECT room_status FROM Room WHERE room_id = " + room_id + " AND room_status = 'T'";
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			if (stmt.executeQuery(query).next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			super.closeConn();
+		}
 		return false;
 	}
 
-	public boolean checkRoom(int room_id, ArrayList<Long> users) {
-		super.connect();
+	public boolean checkAttendance(String room_id, ArrayList<Long> users) {
+		// check minimum and maximum attendance
+		String query = "SELECT min_user, max_user FROM Room WHERE room_id = " + room_id;
 		try {
+			super.connect();
 			Statement stmt = conn.createStatement();
-
-			// check room status
-			String query1 = "SELECT room_status FROM Room WHERE room_id = " + room_id + " AND room_status = 'T'";
-			if (!stmt.execute(query1)) {
-				return false;
-			}
-
-			for (long user_id : users) {
-				// check user status
-				if (!checkUserStatus(user_id)) {
-					return false;
-				}
-
-				// 每人於二週內至多可申請三個時段
-				java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
-				String query2 = "SELECT COUNT(order_id) FROM Orders AS o INNER JOIN OrderUsers AS u ON o.order_id = u.order_id "
-						+ "WHERE user_id = " + user_id + " AND is_lender = 'T' AND order_status != 'cancelled'"
-						+ " AND DATEDIFF(" + date + ", order_date) <= 14 HAVING COUNT(order_id) = 3";
-				if (stmt.execute(query2)) {
-					return false;
-				}
-			}
-			
-			// 每一時段以四小時為上限???
-
-			// check minimum and maximum attendance
-			String query3 = "SELECT min_user, max_user FROM Room WHERE room_id = " + room_id;
-			if (stmt.execute(query3)) {
+			if (stmt.execute(query)) {
 				ResultSet result = stmt.getResultSet();
 				int min = result.getInt("min_user");
 				int max = result.getInt("max_user");
@@ -329,5 +331,86 @@ public class Order extends ConnectDb {
 			super.closeConn();
 		}
 		return false;
+	}
+
+	public String checkSeat(String seat_id, long user_id) {
+		String errMsg = "Not available: ";
+		// check seat status
+		if (!checkSeatStatus(seat_id)) {
+			errMsg += seat_id + ", ";
+		}
+
+		// check user status
+		if (!checkUserStatus(user_id) || blck.isBanned(user_id)) {
+			errMsg += user_id + ", ";
+		}
+
+		// return error message
+		if (errMsg.equals("Not available: ")) {
+			return "true";
+		} else {
+			return errMsg.substring(0, errMsg.length() - 2);
+		}
+	}
+
+	public String checkRoom(String room_id, ArrayList<Long> users) {
+		String errMsg1 = "Not available: ";
+		// check room status
+		if (!checkRoomStatus(room_id)) {
+			errMsg1 += room_id + ", ";
+		}
+		// check user status
+		for (Long user_id : users) {
+			if (!checkUserStatus(user_id) || blck.isBanned(user_id)) {
+				errMsg1 += user_id + ", ";
+			}
+		}
+
+		// check minimum and maximum attendance
+		String errMsg2 = "true";
+		if (!checkAttendance(room_id, users)) {
+			errMsg2 = "Fail to meet the attendance constraint.";
+		}
+
+		// return error message
+		if (!errMsg1.equals("Not available: ")) {
+			errMsg1 = errMsg1.substring(0, errMsg1.length()-2);
+			if (errMsg2.equals("true")) {
+				return errMsg1;
+			} else {
+				return errMsg1 + "\n" + errMsg2;
+			}
+		} else {
+			return errMsg2;
+		}
+	}
+
+	public String[] select(String select, String db, String attr, String value) {
+		// String db = Orders or OrderUsers
+		// String select = one attribute that you want to select
+		String count = "SELECT COUNT(order_id) FROM " + db + " WHERE " + attr + " = '" + value + "'";
+		String query = "SELECT " + select + " FROM " + db + " WHERE " + attr + " = '" + value + "'";
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			ResultSet result = stmt.executeQuery(count);
+			int size = 0;
+			if (result.next()) {
+				size = result.getInt(1);
+			}
+			String[] retVal = new String[size];
+			int i = 0;
+			result = stmt.executeQuery(query);
+			while (result.next()) {
+				retVal[i] = result.getString(1);
+				i++;
+			}
+			return retVal;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			super.closeConn();
+		}
+		return null;
 	}
 }
--- src/Entity/User.java
+++ src/Entity/User.java
@@ -4,21 +4,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Date;
 
 public class User extends ConnectDb {
 	Blacklist blcklst = new Blacklist();
-	
+
 	public User() {
 		blcklst.renew();
 	}
 
 	public String getUser(long id) {
-		super.connect();
 		String name = null;
 		String sql = "SELECT user_name from User " + "where user_id = ?";
 		try {
+			super.connect();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setLong(1, id);
 
@@ -35,13 +37,61 @@ public class User extends ConnectDb {
 		}
 		return name;
 	}
-	
-	public boolean checkManager(long id, String pwd) {
-		super.connect();
-		String query = "SELECT mngr_id , mngr_psswrd FROM Manager WHERE mngr_id = " + id + " AND mngr_psswrd= '" + pwd + "'";
+
+	public ArrayList<Long> getOrders(long id) {
+		ArrayList<Long> oid = new ArrayList<Long>();
+		String query = "SELECT order_id FROM OrderUsers AS o INNER JOIN User AS u ON o.user_id = u.user_id WHERE u.user_id = "
+				+ id;
 		try {
+			super.connect();
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
+			stmt.execute(query);
+			ResultSet result = stmt.getResultSet();
+			while (result.next()) {
+				oid.add(result.getLong("order_id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			super.closeConn();
+		}
+		return oid;
+	}
+
+	public String getOrderInfo(long oid) {
+		String retVal = "";
+		String query = "SELECT seat_id, order_date, borrow_start, borrow_end FROM Orders WHERE order_id = "
+				+ oid;
+		try {
+			super.connect();
+			Statement stmt = conn.createStatement();
+			stmt.execute(query);
+			ResultSet result = stmt.getResultSet();
+			if (result.next()) {
+				retVal = oid + "   ";
+				if (result.getString("seat_id") != null) {
+					retVal += "地點：" + result.getString("seat_id");
+				}
+				retVal += "   時間：" + result.getString("order_date") + " " + result.getInt("borrow_start") + "~"
+						+ result.getInt("borrow_end");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			super.closeConn();
+		}
+		return retVal;
+	}
+	
+	public boolean managerLogin(long id, String pwd) {
+		String sql = "SELECT mngr_id , mngr_psswrd FROM Manager WHERE mngr_id = ? AND mngr_psswrd = ?";
+		try {
+			super.connect();
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			pstmt.setString(2, pwd);
+			
+			ResultSet rs = pstmt.executeQuery();
 			int count = 0;
 			while (rs.next()) {
 				count++;
@@ -60,16 +110,15 @@ public class User extends ConnectDb {
 		}
 	}
 
-	public boolean checkUser(long id, String pwd) {
-		super.connect();
-		String sql = "SELECT user_id , user_psswrd from User " + "where user_id = ? and user_psswrd= ?";
+	public boolean userLogin(long id, String pwd) {
+		String sql = "SELECT user_id , user_psswrd FROM User " + "WHERE user_id = ? AND user_psswrd = ?";
 		try {
+			super.connect();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setLong(1, id);
 			pwd = base64Encode(pwd);
 			pstmt.setString(2, pwd);
 
-			// Query
 			ResultSet rs = pstmt.executeQuery();
 			int count = 0;
 			while (rs.next()) {
@@ -91,59 +140,12 @@ public class User extends ConnectDb {
 		}
 	}
 
-	public ArrayList<Long> getOrders(long id) {
-		super.connect();
-		ArrayList<Long> oid = new ArrayList<Long>();
-		String query = "SELECT order_id FROM OrderUsers AS o INNER JOIN User AS u ON o.user_id = u.user_id WHERE u.user_id = "
-				+ id;
-		try {
-			Statement stmt = conn.createStatement();
-			stmt.execute(query);
-			ResultSet result = stmt.getResultSet();
-			while (result.next()) {
-				oid.add(result.getLong("order_id"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.closeConn();
-		}
-		return oid;
-	}
-
-	public String getOrderInfo(long oid) {
-		super.connect();
-		String retVal = "";
-		String query = "SELECT room_id, seat_id, order_date, borrow_start, borrow_end FROM Orders WHERE order_id = "
-				+ oid;
-		try {
-			Statement stmt = conn.createStatement();
-			stmt.execute(query);
-			ResultSet result = stmt.getResultSet();
-			if (result.next()) {
-				retVal = oid + "   ";
-				if (result.getString("seat_id") != null) {
-					retVal += "地點：" + result.getString("seat_id");
-				} else if (result.getInt("room_id") != 0) {
-					retVal += "地點：  " + result.getInt("room_id");
-				}
-				retVal += "   時間：" + result.getString("order_date") + " " + result.getInt("borrow_start") + "~"
-						+ result.getInt("borrow_end");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.closeConn();
-		}
-		return retVal;
-	}
-
 	public boolean createUser(String name, long id, String pwd) {
-		super.connect();
 		String sql = "INSERT INTO " + "User(user_id, user_name , user_psswrd , user_status , violations) "
 				+ "VALUES(?,?,?,?,?)";
 
 		try {
+			super.connect();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setLong(1, id);
 			pstmt.setString(2, name);
@@ -175,7 +177,7 @@ public class User extends ConnectDb {
 		// "志明", 109405094, "haha"
 		// "春嬌", 109405088, "hehe"
 		// "龐德", 109405007, "007"
-		
+
 		boolean b = user.createUser("Mary", 109306099, "abcd");
 		if (b) {
 			System.out.println("新增使用者 :成功 ");
@@ -184,13 +186,13 @@ public class User extends ConnectDb {
 		}
 
 		// 測試登入
-		b = user.checkUser(109306099, "abcd1");
+		b = user.userLogin(109306099, "abcd1");
 		if (b) {
 			System.out.println("使用者登入(錯誤密碼) :成功 ");
 		} else {
 			System.out.println("使用者登入(錯誤密碼) :失敗 ");
 		}
-		b = user.checkUser(109306099, "abcd");
+		b = user.userLogin(109306099, "abcd");
 		if (b) {
 			System.out.println("使用者登入(正確密碼) :成功 ");
 		} else {
